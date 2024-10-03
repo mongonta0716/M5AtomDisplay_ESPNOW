96e02d29c4b7530ca494d80a327618a12ba304c5
Takao Akaki <mongonta@gmail.com>
2024-10-03 15:17:37 +0900

    ScrollTest

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <Arduino.h>
+#include <M5AtomDisplay.h>
 #include <M5Unified.h>
 
+M5AtomDisplay display(240, 120, 120, 480, 320, 2, 2, 74250000);
+
+String hello_str = "こんにちは、私はスタックチャンです。スーパーかわいい手乗りロボットとしてあなたのそばにいます。";
+
+
+
+
 void setup() {
+  display.init();
+  delay(5000);
+  display.setFont(&fonts::lgfxJapanGothic_40);
+
 
 }
 
 void loop() {
-    
+  display.startWrite();
+
+  for (int i = display.width(); i > -display.textWidth(hello_str); --i)
+  {
+    display.drawString(hello_str, i, (display.height() - display.fontHeight()) >> 1);
+  }
+  display.endWrite();
+  delay(20);
 }
